1cf67b4a0f9eae0d8bcc16d3f0bdcf67e9264cce
ekrem.deger15@gmail.com <ekrem.deger15@gmail.com>
2019-06-04 03:03:52 +0300

    working on loading spinner -- not working

--- XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using XamarinApp.Managers;
@@ -9,10 +10,13 @@ using XamarinApp.Managers;
 namespace XamarinApp {
     public partial class MainPage : ContentPage {
         public MainPage() {
+            this.IsBusy = true;
+            Thread.Sleep(5 * 1000);
             InitializeComponent();
             var result = new BookManager().GetBooks();
             if(!result.Item1) ErrorMessage.Text = result.Item2;
             Books.BindingContext = result.Item3;
+            this.IsBusy = false;
         }
     }
 }
